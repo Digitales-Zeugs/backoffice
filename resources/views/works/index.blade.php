47ada7d68c16194ef9544449bc2bc6348bbc5446
Roberto Vaccaro <roberto.vaccaro@qkstudio.com>
2020-10-05 17:20:12 -0300

    Datatables para solicitud registro de obra

--- resources/views/works/index.blade.php
+++ resources/views/works/index.blade.php
@@ -1,44 +1,106 @@
 @extends('dashboard.layout')
 
 @section('content')
-<section class="content-header">
-<h1>Solicitudes de Registro de Obra</h1>
-</section>
-<section class="content">
-@if(count($requests) > 0)
-<table class="table table-hover">
-    <tr>
-        <th>#</th>
-        <th>Título</th>
-        <th>DNDA Letra</th>
-        <th>DNDA Música</th>
-        <th>Fecha</th>
-    </tr>
-@foreach($requests as $request)
-    <tr class="clickable" data-id="{{ $request->id }}" style="cursor: pointer">
-        <td>{{ $request->id }}</td>
-        <td>{{ $request->title }}</td>
-        <td>{{ $request->lyric_dnda_file }}</td>
-        <td>{{ $request->audio_dnda_file }}</td>
-        <td>{{ $request->created_at->format('d/m/Y H:i') }}</td>
-    </tr>
-@endforeach
-</table>
-@else
-<div class="alert alert-warning" role="alert">
-  No hay solicitudes pendientes.
+<div class="container">
+    <section class="content-header">
+        <h1>Solicitudes de Registro de Obra</h1>
+    </section>
+    <section class="content">
+    @if(count($requests) > 0)
+    <table class="table">
+        <thead>
+            <tr>
+                <th></th>
+                <th>#</th>
+                <th>Título</th>
+                <th>DNDA Letra</th>
+                <th>DNDA Música</th>
+                <th>Fecha</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    @else
+    <div class="alert alert-warning" role="alert">
+    No hay solicitudes pendientes.
+    </div>
+    @endif
+    </section>
 </div>
-@endif
-</section>
 @endsection
 
 @push('scripts')
 <script>
 window.onload = function() {
-    jQuery('.clickable').on('click', function(event) {
-        window.location = `/profiles/${this.dataset.id}`;
+    const data = {!! json_encode($requests) !!};
+    const roles = {!! json_encode($roles) !!};
+
+    console.log(roles);
+
+    const format = function( register ) {
+        let output = '<table cellpadding="5" cellspacing="0" border="0" style="margin-left:50px;">';
+        output += '<tr>';
+        output += '<th>Función</th>';
+        output += '<th>Apellido y Nombre</th>';
+        output += '<th>DNI</th>';
+        output += '<th>Socio</th>';
+        output += '<th>Consentimiento</th>';
+        output += '</tr>';
+        register.distribution.forEach(function(current) {
+            const role = roles.find(r => r.code == current.function);
+
+            output += '<tr>';
+            output += `<td>${role.description}</td>`;
+            output += `<td>${current.name}</td>`;
+            output += `<td>${current.dni}</td>`;
+            output += `<td>${current.member}</td>`;
+            output += `<td>${current.updated_at}</td>`;
+            output += '</tr>';
+        })
+        output += '</table>';
+
+        return output;
+    };
+
+    const table = $('.table').DataTable({
+        "data": data,
+        "columns": [
+            {
+                "className":      'details-control',
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": ''
+            },
+            { "data": "id" },
+            { "data": "title" },
+            { "data": "lyric_dnda_file" },
+            { "data": "audio_dnda_file" },
+            { "data": "created_at" }
+        ]
+    });
+
+    $('.table tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row( tr );
+ 
+        if ( row.child.isShown() ) {
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            row.child( format(row.data()) ).show();
+            tr.addClass('shown');
+        }
     });
 }
 </script>
 @endpush
-
