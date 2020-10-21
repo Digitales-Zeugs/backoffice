e87a2d24ab92e08469542cdb8279db7fdf44ce64
Roberto Vaccaro <roberto.vaccaro@qkstudio.com>
2020-10-21 15:36:58 -0300

    Agregado estados para registro socios

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(MemberSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(WorkStatusSeeder::class);
+        $this->call(MemberRegistrationStatusSeeder::class);
     }
 }
