f3ec4ebfd27eb796d6000e25c2d5b74714c00585
Roberto Vaccaro <roberto.vaccaro@qkstudio.com>
2020-10-23 16:33:15 -0300

    Nuevo usuario pruebas

--- src/database/seeds/UserSeeder.php
+++ src/database/seeds/UserSeeder.php
@@ -15,9 +15,20 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name'              => 'Juan Prueba',
+            'name'              => 'Roberto Prueba',
             'phone'             => '542215751688',
             'email'             => 'roberto.vaccaro@qkstudio.com',
+            'email_verified_at' => now(),
+            'password'          => Hash::make('pruebas'),
+            'remember_token'    => Str::random(32),
+            'created_at'        => now(),
+            'updated_at'        => now()
+        ]);
+
+        DB::table('users')->insert([
+            'name'              => 'Leo Prueba',
+            'phone'             => '542215751688',
+            'email'             => 'leo.miaton@qkstudio.com',
             'email_verified_at' => now(),
             'password'          => Hash::make('pruebas'),
             'remember_token'    => Str::random(32),
