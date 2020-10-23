784e55be6e9fa570118c26cc3e606c8f47759b06
Roberto Vaccaro <roberto.vaccaro@qkstudio.com>
2020-10-23 14:54:58 -0300

    Control de permisos desde el model User

--- src/app/User.php
+++ src/app/User.php
@@ -33,4 +33,37 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Privileges', 'recid_usuario', 'recid');
     }
+
+    public function can($abilities, $arguments = [])
+    {
+        $validPermissions = ['sin permisos', 'lee', 'carga', 'homologa'];
+
+        // El valor solicitado no está entre los permisos válidos
+        if (!in_array($arguments, $validPermissions)) {
+            return false;
+        }
+
+        $value = optional($this->privileges->firstWhere('capitulo', $abilities))->privilegios;
+
+        // No se encontró el capítulo
+        if (!$value) {
+            return false;
+        }
+
+        // El permiso devuelto no estrá entre los permisos válidos
+        if (!in_array($value, $validPermissions)) {
+            return false;
+        }
+
+        // Obtengo la posición en la "escala" de permisos
+        $pos_expected = array_search($arguments, $validPermissions);
+        $pos_value = array_search($value, $validPermissions);
+
+        // El permiso solicitado es mayor que el permiso devuelto
+        if ($pos_expected > $pos_value) {
+            return false;
+        }
+
+        return true;
+    }
 }
