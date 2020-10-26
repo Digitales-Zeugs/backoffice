9beec335ff6a192b7dcb7d88c66ac9e0d3031937
Roberto Vaccaro <roberto.vaccaro@qkstudio.com>
2020-10-26 10:12:20 -0300

    Notificación de vencimiento

--- src/app/Console/Commands/SADAICExpirationCommand.php
+++ src/app/Console/Commands/SADAICExpirationCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Mail\NotifyExpiration;
 use App\Models\Work\Log as InternalLog;
+use App\Models\Work\Registration;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -23,24 +25,33 @@ class SADAICExpirationCommand extends Command
         $date = date_create();
         date_sub($date, date_interval_create_from_date_string("15 days"));
 
-        $expired = DB::table('works_registration')
-        ->where([
-            ['status_id', 3],
-            ['updated_at', '<=', date_format($date, "Y-m-d")]
-        ])->get();
+        // Registros en proceso o en disputa sin cambios en los últimos 15 días
+        $expired = Registration::where('updated_at', '<=', date_format($date, "Y-m-d"))
+        ->whereIn('status_id', [2, 3])
+        ->get();
 
         $expired->each(function ($item, $key) {
+            // Registramos el cambio en el log
             InternalLog::create([
                 'registration_id' => $item->id,
                 'action_id'       => 13, // REQUEST_EXPIRED
                 'time'            => now()
             ]);
 
-            DB::table('works_registration')
-            ->where('id', $item->id)
-            ->update([
-                'status_id' => 4 // Vencido
-            ]);
+            // Actualizamos el estado del trámite
+            $item->status_id = 4; // Vencido
+            $item->save();
+
+            foreach($item->distribution as $distribution) {
+                if ($distribution->type != 'member') {
+                    continue;
+                }
+
+                if (trim($distribution->member->email) != "" && filter_var($distribution->member->email, FILTER_VALIDATE_EMAIL)) {
+                    // Si tiene dirección válida, notificamos
+                    Mail::to($distribution->member->email)->queue(new NotifyExpiration($distribution));
+                }
+            }
         });
 
         return 0;
