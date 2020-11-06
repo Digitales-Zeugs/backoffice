4a69930a2cfa5576c66e43ec5f2033ff72462a1d
Roberto Vaccaro <roberto.vaccaro@qkstudio.com>
2020-11-06 09:14:50 -0300

    Fix: Nombre en las notificaciones

--- src/resources/views/mails/notify-finalization.blade.php
+++ src/resources/views/mails/notify-finalization.blade.php
@@ -1,3 +1,7 @@
+@if ($registration->initiator->member_id)
+<p>¡Hola {{ $registration->initiator->nombre }}!</p>
+@else
 <p>¡Hola {{ $registration->initiator->name }}!</p>
+@endif
 
 Notificación de Finalización de Solicitud de Registro de Obra
