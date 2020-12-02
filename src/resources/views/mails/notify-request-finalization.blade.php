09cdec0cad7c857a17498cf13a77764b8699395f
Roberto Vaccaro <roberto.vaccaro@qkstudio.com>
2020-12-02 11:39:24 -0300

    Template y CTA para los mails

--- src/resources/views/mails/notify-request-finalization.blade.php
+++ src/resources/views/mails/notify-request-finalization.blade.php
@@ -1,3 +1,10 @@
-<p>¡Hola {{ $nombre }}!</p>
+@extends('mails.template')
 
-Notificación de Vencimiento de Solicitud de Registro de Obra
+@section('content')
+<div class="kicker">Solicitud de Registro de Obra</div>
+<p>¡Hola {{ $nombre }}!</p>
+<p>Una solicitud de Registro de Obra de la que sos parte <strong>FINALIZÓ</strong>, para ver más información accedé al sitio de autogestión de SADAIC:</p>
+<div class="cta">
+    <a class="btn" href="{{ route('login') }}">Ir a la Autogestión</a>
+</div>
+@endsection
