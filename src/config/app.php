4217ae893d193c56fefdcc7bdc20b6c7cac2c982
Roberto Vaccaro <roberto.vaccaro@qkstudio.com>
2021-02-26 10:28:55 -0300

    Version bump: 1.8.20

--- src/config/app.php
+++ src/config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Back Office | SADAIC'),
-    'version' => '1.8.19',
+    'version' => '1.8.20',
     'sitio_publico' => env('SP_URL', 'http://10.26.110.101:8000/login'),
 
     /*
